70474d8d8f5adb92e886d67e0916b9c03a3de82c
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-01-18 20:16:00 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const multer = require('multer')
+const path = require('path')
+const serveStatic = require('serve-static')
+const fs = require('fs')
+
+const app = express()
+
+app.use(express.static('frontend'))
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, './uploads');
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+  });
+  
+  const upload = multer({ storage: storage })
+  
+  app.post('/upload', upload.single('image'), (req, res) => {
+    res.send('Image uploaded successfully')
+  })
+
+
+  app.get('/stats', (req, res) => {
+    fs.readdir('./uploads', (err, files) => {
+      if (err) {
+        res.status(500).send('Error reading directory');
+        return
+      }
+  
+      let totalSize = 0
+  
+      files.forEach(file => {
+        totalSize += fs.statSync('./uploads/' + file).size
+      });
+  
+      res.send({
+        numberOfImages: files.length,
+        totalSize: totalSize
+      })
+    })
+  })
+
+  app.use('/uploads', serveStatic(path.join(__dirname, 'uploads')))
+
+  app.listen(3000, () => console.log('Server started on port 3000'))
